03c92880d9fd12f11c71a74fde80cb84dac625cf
Rajan kumar <krcpr007@gmail.com>
2022-07-20 13:47:41 +0530

    footer made

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+function Footer() {
+    return (
+        <>
+            <div className='bottom-0 w-full bg-[#222831]'>
+                <h1 className='text-[#00ADB5] text-xl text-center pt-4'>All credits reserved to @krcpr007</h1>
+            </div>
+        </>
+    )
+}
+
+export default Footer
