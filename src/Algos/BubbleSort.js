332701d4adc4a19597e8c866e0b4c2c25f03e44e
Rajan kumar <krcpr007@gmail.com>
2022-08-05 22:43:52 +0530

    comments added

--- src/Algos/BubbleSort.js
+++ src/Algos/BubbleSort.js
@@ -12,23 +12,23 @@ const BubbleSort = () =>{
    const solve = () => {
       
       for(let i = values.length,timer = 0; i > 0;timer += i-1, i--){
-         setTimeout(() => {
+         setTimeout(() => {  // this is setTimeout function it used to schedule innermost for loop ; 
             for(let j = 1; j < i; j++){
-               setTimeout(() => {
+               setTimeout(() => { // this is setTimeout function it used to compare too elements ; 
                   document.getElementById(ids[j]).childNodes[1].style.backgroundColor = 'black';
                   document.getElementById(ids[j-1]).childNodes[1].style.backgroundColor = 'black';
                   
-                  setTimeout(() => {
+                  setTimeout(() => { // this is setTimeout function it used to regain the original color  ; 
                      document.getElementById(ids[j]).childNodes[1].style.backgroundColor = myState.color;
                      document.getElementById(ids[j-1]).childNodes[1].style.backgroundColor = myState.color;
                   },myState.speed-10);
                      
-                  if(values[j]<values[j-1]){
-                     let temp = values[j];
+                  if(values[j]<values[j-1]){ //nothing just swapping values
+                     let temp = values[j]; 
                      values[j] = values[j-1];
                      values[j-1] = temp;
 
-                     temp = ids[j];
+                     temp = ids[j];   //swapping ids
                      ids[j] = ids[j-1];
                      ids[j-1] = temp;
                      
